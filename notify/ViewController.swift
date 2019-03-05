116ddf0ed67cc643f5d1f560d0a2cda896287492
Kayla Butler <kaylabutler@kaylabutler-macbookpro.roam.corp.google.com>
2019-03-04 18:26:01 -0800

    First Commit :)

--- notify/ViewController.swift
+++ notify/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current().delegate = (self as! UNUserNotificationCenterDelegate)
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { (granted, error) in
+            guard granted else {// 1. Check if permission granted
+            
+            return
+            }
+            print("Permission granted: \(granted)")
+            
+            // 2. Attempt registration for remote notifications on the main thread
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        })
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerForPushNotifications()
+        return true
     }
 
 
